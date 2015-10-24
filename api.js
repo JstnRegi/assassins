2191c6d4d267c005637cc7454b9e18ace9889dd4
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-24 11:31:38 -0700

    added endpoint for /api/games

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 var express = require('express');	
 var	api = express.Router();
 var adminsCtrl = require('./controllers/admins_controller.js');
+var gamesCtrl = require('./controllers/games_controller.js');
 
 // API ROUTES
 
@@ -9,5 +10,8 @@ api.post('/api/admin/register', adminsCtrl.register);
 api.post('/api/admin/login', adminsCtrl.login);
 api.get('/api/admin/logout', adminsCtrl.logout);
 api.get('/api/admin', adminsCtrl.currentAdmin);
+
+//games
+api.post('/api/games', gamesCtrl.create);
 
 module.exports = api;
