df75bface14edcb8ec06d3aa0056623d8f7b361c
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-24 12:44:50 -0700

    export game create method that creates a game in the db

--- controllers/games_controller.js
+++ controllers/games_controller.js
@@ -2,5 +2,17 @@ var express = require('express'),
     Game = require('./../models').Game;
 
 module.exports.create = function(req, res) {
-	
+	console.log("games_controller.js", req.body);
+	var game = req.body;
+	Game.create(game, function(err, game) {
+		if (err) {
+			console.log(err);
+			res.status(500).json({err: err})
+		} else {
+			res.status(200).json({
+				status: "Game creation successful",
+				data: game
+			});
+		}
+	})
 }
