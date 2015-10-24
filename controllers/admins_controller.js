7062a2aeaf6f767d8c2d1644b1b613cbe6642133
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-24 00:25:44 -0700

    added login and logout module exports

--- controllers/admins_controller.js
+++ controllers/admins_controller.js
@@ -1,6 +1,9 @@
 var express = require('express'),
     Admin = require('./../models').Admin;
 
+
+
+
 // signup, admins#create
 module.exports.register = function (req, res) {
 	console.log(req.body);
@@ -11,6 +14,7 @@ module.exports.register = function (req, res) {
 			res.status(500).json({err: err});
 		}
 		else {
+			req.login(admin);
 			res.status(200).json({
 				status: "Registration successful",
 				data: admin
@@ -18,3 +22,36 @@ module.exports.register = function (req, res) {
 		}
 	});
 };
+
+module.exports.login = function(req, res) {
+	var adminInfo = req.body;
+	Admin.authenticate(adminInfo.username, adminInfo.password, function(err, admin) {
+		if(err) {
+			console.log(err);
+			res.status(500).json({err: err});
+		}
+		else {
+			req.login(admin);
+			req.currentAdmin(function(err, admin) {
+				if (err) {
+					return console.log(err);
+				}
+				console.log(admin);
+			});
+			res.status(200).json({
+				status: "Login successful",
+				data: admin
+			});
+		}
+	});
+};
+
+// signout, sessions#destroy
+module.exports.logout = function (req, res) {
+  req.logout();
+  res.status(200).json({status: 'Bye!'});
+};
+
+
+
+
