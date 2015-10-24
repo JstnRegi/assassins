f54cb609dd176317260a1128da3be302fd6d339c
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-24 00:30:34 -0700

    added adminLogin and main Ctrl

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,12 +1,25 @@
 var app = angular.module('myApp.controllers', []);
 
 //make mainCtrl
+app.controller('mainCtrl',
+  ['$scope', '$location', 'AuthService', '$window',
+  function ($scope, $location, AuthService, $window) {
+
+    //set admin
+    $scope.admin = $window.admin;
+    console.log(AuthService.isLoggedIn());
+}]);
 
 // CONTROLLERS
-app.controller('adminRegisterController',
+
+// AUTH CONTROLLERS 
+app.controller('adminRegisterCtrl',
   ['$scope', '$location', 'AuthService',
   function ($scope, $location, AuthService) {
   	$scope.newAdmin = {};
+
+    //initial value
+    $scope.error = false
 
   	$scope.register = function() {
   		AuthService.register($scope.newAdmin)
@@ -19,5 +32,45 @@ app.controller('adminRegisterController',
         $scope.newAdmin.password = null;
       })
   	};
+}]);
+
+app.controller('adminLoginCtrl',['$scope','$rootScope', '$location', 'AuthService',
+ function ($scope, $rootScope, $location, AuthService) {
+    $scope.loginForm = {};
+
+    $scope.login = function () {
+      //initial value
+      $scope.error = false;
+
+      //call login from service
+      AuthService.adminLogin($scope.loginForm)
+      // handle success
+      .then(function() {
+        $location.path('/');
+        $scope.loginForm = {};
+      })
+      // handle error
+      .catch(function () {
+        $scope.error = true;
+        $scope.errorMessage = "Invalid username and/or password";
+        $scope.loginForm.password = null;
+      })
+    }; 
+}]);
+
+app.controller('logoutCtrl', ['$scope', '$location', 'AuthService', function ($scope, $location, AuthService) {
+
+  console.log('logoutCtrl');
+    $scope.logout = function () {
+      // call logout from service
+      AuthService.logout()
+        .then(function () {
+          $location.path('/');
+        });
+
+    };
 
 }]);
+
+
+
