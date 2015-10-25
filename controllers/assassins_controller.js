ef30c541e0b90237c68e9930b45588f3845c28b3
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-25 13:03:13 -0700

    changed game argument to game._id in createSecure func call

--- controllers/assassins_controller.js
+++ controllers/assassins_controller.js
@@ -14,7 +14,7 @@ module.exports.register = function (req, res) {
 			return console.log(err);
 		} else {
 			if(game !== null) {
-				Assassin.createSecure(assassin.codename, assassin.password, assassin.avatar, assassin.real_photo, assassin.tagline, game, function(err, assassin) {
+				Assassin.createSecure(assassin.codename, assassin.password, assassin.avatar, assassin.real_photo, assassin.tagline, game._id, function(err, assassin) {
 					if(err) {
 						console.log(err);
 						res.status(500).json({err: "Another assassin has already taken that codename.",
