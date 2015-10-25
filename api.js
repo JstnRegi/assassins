f5f208a871595d19d3e1dcc9554b23ec3468db95
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-24 17:42:58 -0700

    added assassins/:gameTitle endpoint. required assassin ctrl

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var	api = express.Router();
 var adminsCtrl = require('./controllers/admins_controller.js');
 var gamesCtrl = require('./controllers/games_controller.js');
+var assassinsCtrl = require('./controllers/assassins_controller.js');
 
 // API ROUTES
 
@@ -12,7 +13,10 @@ api.get('/api/admin/logout', adminsCtrl.logout);
 api.get('/api/admin', adminsCtrl.currentAdmin);
 
 //games
-api.post('/api/game', gamesCtrl.find)
+api.post('/api/game', gamesCtrl.find);
 api.post('/api/games', gamesCtrl.create);
+
+//assassins
+api.post('/api/assassins/:gameTitle', assassinsCtrl.register);
 
 module.exports = api;
