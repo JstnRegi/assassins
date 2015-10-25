509572d6bc70dc9eead26012ce132d4a501a2b64
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-24 17:47:03 -0700

    created assassin model

---
+++ models/assassin.js
@@ -0,0 +1,89 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+
+var AssassinSchema = new Schema ({
+	codename: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	tagline: {
+		type: String,
+		required: true
+	}, 
+	passwordDigest: {
+		type: String,
+		required: true
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	},
+	avatar: {
+		type: String,
+		required: true
+	},
+	real_photo: {
+		type: String,
+		required: true
+	},
+	game: {
+		type: String
+	},
+	is_alive: {
+		type: Boolean
+	},
+	died_on: {
+		type: Date
+	},
+	kills: [],
+	target: {
+		type: String
+	},
+	kill_submissions: [], //stretch
+	posts: []
+});
+
+AssassinSchema.statics.createSecure = function(codename, password, avatar, real_photo, tagline, game,  cb) {
+	// _this references our schema. Not to be confused with the primitive value 'this' later on during the function
+	var _this = this;
+	bcrypt.genSalt(function (err, salt) {
+		//hash the password with salt
+		bcrypt.hash(password, salt, function(err, hash) {
+			var assassin = {
+				codename: codename,
+				passwordDigest: hash,
+				avatar: avatar,
+				real_photo: real_photo,
+				tagline: tagline,
+				game: game,
+				createdAt: Date.now() 
+			};
+			_this.create(assassin, cb);
+		});
+	});
+};
+
+AssassinSchema.statics.authenticate = function(codename, password, cb) {
+    this.findOne({codename: codename}, function(err, assassin) {
+    	if(err) {
+    		return console.log(err);
+    	}
+        if(admin === null) {
+            cb('Can\'t find user with that codename', null);
+        } else if(admin.checkPassword(password)) {
+            cb(null, admin);
+        } else {
+            cb('password incorrect', admin)
+        }
+    });
+};
+
+AssassinSchema.methods.checkPassword = function(password) {
+    return bcrypt.compareSync(password, this.passwordDigest);
+};
+
+var Assassin = mongoose.model('Assassin', AssassinSchema);
+
+module.exports = Assassin;
